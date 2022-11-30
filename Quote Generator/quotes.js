76889321e2dc1a0a25037292d959f1e32f3004cd
kimmingye <dusgml3779@gmail.com>
2022-12-01 08:29:24 +0900

    Add javascript Functions

---
+++ Quote Generator/quotes.js
@@ -0,0 +1,82 @@
+const localQuotes = [{
+  text: 'The years teach much which the days never know.',
+  author: 'Ralph Emerson'
+}, {
+  text: 'Our distrust is very expensive.',
+  author: 'Ralph Emerson'
+}, {
+  text: 'All know the way; few actually walk it.',
+  author: 'Bodhidharma'
+}, {
+  text: 'Great talent finds happiness in execution.',
+  author: 'Johann Wolfgang von Goethe'
+}, {
+  text: 'Faith in oneself is the best and safest course.',
+  author: 'Michelangelo'
+}, {
+  text: 'Courage is going from failure to failure without losing enthusiasm.',
+  author: 'Winston Churchill'
+}, {
+  text: 'The two most powerful warriors are patience and time.',
+  author: 'Leo Tolstoy'
+}, {
+  text: 'Anticipate the difficult by managing the easy.',
+  author: 'Lao Tzu'
+}, {
+  text: 'Those who are free of resentful thoughts surely find peace.',
+  author: 'Buddha'
+}, {
+  text: 'A short saying often contains much wisdom.',
+  author: 'Sophocles'
+}, {
+  text: 'It takes both sunshine and rain to make a rainbow.',
+  author: null
+}, {
+  text: 'A beautiful thing is never perfect.',
+  author: null
+}, {
+  text: 'Only do what your heart tells you.',
+  author: 'Princess Diana'
+}, {
+  text: 'Life is movement-we breathe, we eat, we walk, we move!',
+  author: 'John Pierrakos'
+}, {
+  text: 'No one can make you feel inferior without your consent.',
+  author: 'Eleanor Roosevelt'
+}, {
+  text: 'Argue for your limitations, and sure enough theyre yours.',
+  author: 'Richard Bach'
+}, {
+  text: 'Luck is what happens when preparation meets opportunity.',
+  author: 'Seneca'
+}, {
+  text: 'Victory belongs to the most persevering.',
+  author: 'Napoleon Bonaparte'
+}, {
+  text: 'Love all, trust a few, do wrong to none.',
+  author: 'William Shakespeare'
+}, {
+  text: 'In order to win, you must expect to win.',
+  author: 'Richard Bach'
+}, {
+  text: 'A goal is a dream with a deadline.',
+  author: 'Napoleon Hill'
+}, {
+  text: 'You can do it if you believe you can!',
+  author: 'Napoleon Hill'
+}, {
+  text: "Set your goals high, and don't stop till you get there.",
+  author: 'Bo Jackson'
+}, {
+  text: 'Every new day is another chance to change your life.',
+  author: null
+}, {
+  text: 'Smile, breathe, and go slowly.',
+  author: 'Thich Nhat Hanh'
+}, {
+  text: 'Nobody will believe in you unless you believe in yourself.',
+  author: 'Liberace'
+}, {
+  text: 'Do more than dream: work.',
+  author: 'William Arthur Ward'
+}]
