aff28c891916518137e02be028f655a3695930ab
kimmingye <dusgml3779@gmail.com>
2022-12-27 16:54:19 +0900

    Add Loader

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author field is blank and replace it with unknown
@@ -24,11 +38,14 @@ function newQuote() {
     quoteText.classList.remove('long-quote')
 
   }
+  // Set quote, Hide Looder
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -53,4 +70,4 @@ twitterBtn.addEventListener('click',tweetQuote)
 
 //On Load
 getQuotes();
-//newQuote();
+
